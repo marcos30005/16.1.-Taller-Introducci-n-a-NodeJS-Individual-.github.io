595bcd5e8cf86f4547d1b7ff8b6772988f238c37
marcos30005 <marcoslibertad.mg@gmail.com>
2023-10-19 22:19:32 -0300

    subir

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.use(express.json()); // Permite que el servidor analice el cuerpo de las peticiones como JSON
 
-const people = require("./json/people.json"); // Importa los datos iniciales (generados en https://www.mockaroo.com/)
+let people = require("./json/people.json"); // Importa los datos iniciales (generados en https://www.mockaroo.com/)
 
 app.get("/", (req, res) => {
   // El primer parámetro SIEMPRE es asociado a la request (petición) y el segundo a la response (respuesta)
@@ -38,13 +38,31 @@ app.put("/people/:index", (req, res) => {
   /* COMPLETA EL CÓDIGO NECESARIO:
      Para que se pueda actualizar el objeto asociado al índice indicado en la URL 
    */
-});
+     const index = req.params.index;
+     if (index >= 0 && index < people.length) {
+       // Verificamos que el índice sea válido
+       people[index] = req.body; // Actualizamos el elemento en el índice especificado
+       res.json(people[index]); // Respondemos con el elemento actualizado
+     } else {
+       res.status(404).json({ error: "Índice no válido" }); // Si el índice no es válido, respondemos con un error 404
+     }
+   });
+
 
 app.delete("/people/:index", (req, res) => {
   /* COMPLETA EL CÓDIGO NECESARIO:
      Para que se pueda eliminar el objeto asociado al índice indicado en la URL 
    */
-});
+
+     const index = req.params.index;
+     if (index >= 0 && index < people.length) {
+       // Verificamos que el índice sea válido
+       const deletedPerson = people.splice(index, 1); // Eliminamos el elemento en el índice especificado
+       res.json(deletedPerson[0]); // Respondemos con el elemento eliminado
+     } else {
+       res.status(404).json({ error: "Índice no válido" }); // Si el índice no es válido, respondemos con un error 404
+     }
+   });
 
 // Esta línea inicia el servidor para que escuche peticiones en el puerto indicado
 app.listen(port, () => {
